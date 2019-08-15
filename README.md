c55ff02e87a02ea8a248b1d4dace2ec08e481708
Tsungyu Yu <ytyubox@gmail.com>
2019-08-15 09:29:51 +0800

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# structSwift
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import structSwiftTests
+
+var tests = [XCTestCaseEntry]()
+tests += structSwiftTests.allTests()
+XCTMain(tests)
